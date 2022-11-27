2479201e02577fa7118cc41a3647838964444f5b
Pallavi02-Test <60915081+Pallavi02-Test@users.noreply.github.com>
2022-11-27 08:41:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,69 @@
+const json = {
+  name: 'root',
+  isFolder: true,
+  items: [
+    {
+      name: 'Public',
+      isFolder: true,
+      items: [
+        {
+          name: 'index.js',
+          isFolder: false,
+        },
+        {
+          name: 'index.html',
+          isFolder: false,
+        },
+      ],
+    },
+    {
+      name: 'src',
+      isFolder: true,
+      items: [
+        {
+          name: 'components',
+          isFolder: true,
+          items: [
+            {
+              name: 'TopBar.js',
+              isFolder: false,
+            },
+            {
+              name: 'RowContainer.js',
+              isFolder: false,
+            },
+            {
+              name: 'SideBar.js',
+              isFolder: false,
+            },
+            {
+              name: 'MuiCard.js',
+              isFolder: false,
+            },
+            {
+              name: 'Footer.js',
+              isFolder: false,
+            },
+            {
+              name: 'index.js',
+              isFolder: false,
+            },
+          ],
+        },
+        {
+          name: 'index.js',
+          isFolder: false,
+        },
+        {
+          name: 'app.js',
+          isFolder: false,
+        },
+        {
+          name: 'package.json',
+          isFolder: false,
+        },
+      ],
+    },
+  ],
+};
+export default json;
